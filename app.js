bc1d9559c66101b2b58fe3cf5ae00b5779ec75c0
muhammadYousof <my 7370044@gmail.com>
2023-10-02 12:54:09 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+var username1 = "MyName"
+
+var username2 = "Yousuf"
+
+alert(username1 + " " + username2);
+
+var age = "20 years old"
+
+alert(age)
+
+
+
+var education = "certified Web & App Development"
+
+alert(education)
+
+var Email = "my 7370044 @gmail.com"
+
+alert(Email)
+
+var Javascript = "i am trying to learn from tha book A smarter Way to learn Javascript"
+
+alert(Javascript)
+
+
+alert("Pizza \n Pizz \n Piz \n")
+
+
+var Disigin = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(Disigin)
